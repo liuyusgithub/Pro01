4c639cb484223f872cd0942ee156b1f578a6e40e
Yu6 <441923097@qq.com>
2019-09-01 12:07:27 +0800

    Initial commit

---
+++ Module01/src/Demoo.java
@@ -0,0 +1,6 @@
+/**
+ * @author yu6
+ * @date 2019/8/27
+ */
+public class Demoo {
+}
---
+++ src/Demo.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("1111");
+
+        Map namemap = new HashMap();
+        System.out.println(namemap);
+
+    }
+
+}
